38cfb6e884afe2fd9906ab333157e0583128f1d9
Ian Henry <ianthehenry@gmail.com>
2022-07-30 17:19:03 -0700

    initial attempt

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,40 @@ import {parser} from "./syntax.grammar"
 import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
 import {styleTags, tags as t} from "@lezer/highlight"
 
-export const EXAMPLELanguage = LRLanguage.define({
+export const JanetLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
+        Form: delimitedIndent({closing: ")", align: false}),
+        Tuple: delimitedIndent({closing: "]", align: false}),
+        Array: delimitedIndent({closing: "]", align: false}),
+        Struct: delimitedIndent({closing: "}", align: false}),
+        Table: delimitedIndent({closing: "}", align: false}),
       }),
       foldNodeProp.add({
-        Application: foldInside
+        Form: foldInside,
+        Tuple: foldInside,
+        Array: foldInside,
+        Struct: foldInside,
+        Table: foldInside,
       }),
       styleTags({
         Identifier: t.variableName,
         Boolean: t.bool,
+        Number: t.number,
         String: t.string,
         LineComment: t.lineComment,
-        "( )": t.paren
+        "( )": t.paren,
+        "@[ [ ]": t.squareBracket,
+        "@{ { }": t.brace,
       })
     ]
   }),
   languageData: {
-    commentTokens: {line: ";"}
+    commentTokens: {line: "#"}
   }
 })
 
-export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage)
+export function janet() {
+  return new LanguageSupport(JanetLanguage)
 }
