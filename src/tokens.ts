9ad0db0a9cee16b6f7402d62d11b60b713d3957a
Ian Henry <ianthehenry@gmail.com>
2023-01-14 14:54:13 -0800

    support backtick strings
    This required upgrading dependencies, which somehow broke the ambiguous number
    parsing, so this commit also fixes that.

---
+++ src/tokens.ts
@@ -0,0 +1,28 @@
+import {ExternalTokenizer, ContextTracker} from "@lezer/lr"
+import {backtickQuotedString} from "./parser.terms.js"
+
+const backTick = '`'.charCodeAt(0);
+
+export const backtickString = new ExternalTokenizer((input, stack) => {
+  if (!stack.canShift(backtickQuotedString)) {
+    return;
+  }
+  let backticks = 0;
+  for (; input.next === backTick; input.advance()) {
+    backticks++;
+  }
+  if (backticks === 0) {
+    return;
+  }
+
+  let currentRun = 0;
+  while (currentRun < backticks && input.next !== -1) {
+    if (input.next === backTick) {
+      currentRun++;
+    } else {
+      currentRun = 0;
+    }
+    input.advance();
+  }
+  input.acceptToken(backtickQuotedString);
+}, {contextual: true, fallback: true});
