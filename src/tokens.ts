0158862e6f55751f4abb5dd57def7dd48bdd08e5
Ian Henry <ianthehenry@gmail.com>
2023-01-15 17:07:45 -0800

    parse buffers properly

--- src/tokens.ts
+++ src/tokens.ts
@@ -2,10 +2,14 @@ import {ExternalTokenizer, ContextTracker} from "@lezer/lr"
 import {backtickQuotedString} from "./parser.terms.js"
 
 const backTick = '`'.charCodeAt(0);
+const atSign = '@'.charCodeAt(0);
 
 export const backtickString = new ExternalTokenizer((input, stack) => {
   if (!stack.canShift(backtickQuotedString)) {
     return;
+  }
+  if (input.next === atSign) {
+    input.advance();
   }
   let backticks = 0;
   for (; input.next === backTick; input.advance()) {
